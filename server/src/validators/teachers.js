363e722074803cd521f71de1a1ad43bac3de9b18
KennethLloyd <verastiguelloyd@gmail.com>
2021-03-10 15:21:24 +0800

    Add teachers validator

--- server/src/validators/teachers.js
+++ server/src/validators/teachers.js
@@ -19,8 +19,50 @@ const updateTeacherPageSettings = async (req, res, next) => {
   }
 };
 
+const addTeacher = async (req, res, next) => {
+  const bodySchema = Joi.object({
+    order: Joi.number().required(),
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    status: Joi.boolean().required(),
+  });
+
+  try {
+    const body = await bodySchema.validateAsync(req.body);
+    req.body = body;
+
+    return next();
+  } catch (err) {
+    return res.status(400).send({
+      error: err.details[0].message,
+    });
+  }
+};
+
+const editTeacher = async (req, res, next) => {
+  const bodySchema = Joi.object({
+    order: Joi.number(),
+    firstName: Joi.string(),
+    lastName: Joi.string(),
+    status: Joi.boolean(),
+  });
+
+  try {
+    const body = await bodySchema.validateAsync(req.body);
+    req.body = body;
+
+    return next();
+  } catch (err) {
+    return res.status(400).send({
+      error: err.details[0].message,
+    });
+  }
+};
+
 const teacherValidator = {
   updateTeacherPageSettings,
+  addTeacher,
+  editTeacher,
 };
 
 export default teacherValidator;
