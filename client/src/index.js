e572767fcea766cd20a40b5b7ca6c1b0ec360269
KennethLloyd <verastiguelloyd@gmail.com>
2021-03-14 15:06:43 +0800

    Add minified css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/argon-dashboard-react.css';
+import './assets/css/styles.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
